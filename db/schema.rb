abcac7c8370baf906b66bdb6afbaf621f7781395
Jaxonsvec <jaxonsvec@gmail.com>
2025-11-29 15:33:42 -0600

    Books added, next JPGS

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[8.0].define(version: 2025_11_29_191252) do
-=======
-ActiveRecord::Schema[8.0].define(version: 2025_11_29_160943) do
->>>>>>> 36ef9912a5640839720fc20ac93c2b3f23d64221
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -73,6 +69,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_29_160943) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ratings", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "user_id"
+    t.decimal "value", precision: 3, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_ratings_on_book_id"
+    t.index ["user_id"], name: "index_ratings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -84,14 +90,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_29_160943) do
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  create_table "ratings", force: :cascade do |t|
-    t.integer "book_id", null: false
-    t.integer "user_id"
-    t.decimal "value", precision: 3, scale: 2, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["book_id"], name: "index_ratings_on_book_id"
-    t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
