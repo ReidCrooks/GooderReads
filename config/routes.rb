e11ce745bb09336f4948fd5bc62a038924d4ca61
Reid <reidpcrooks@gmail.com>
2025-12-01 14:02:52 -0600

    ran rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
 
-    #route for adding books to my reads
+  # route for adding books to my reads
   resources :books do
-    resources :readings, only: [:create, :destroy]
+    resources :readings, only: [ :create, :destroy ]
   end
 
   # Devise routes for users
