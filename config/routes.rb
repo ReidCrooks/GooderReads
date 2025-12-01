b0fa20f900dad033be16ee43a5291bad197e72d7
RaydenBobertson <braydenjamesrobertson@gmail.com>
2025-12-01 12:38:23 -0600

    Fixed issues with login page CSS

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,19 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+
+  #Devise routes for users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  root "pages#home"
+  get "home", to: "pages#home" # redirects /home to root path
+  resources :books
+
+  # Defines the root path route ("/")
+  get "books" => "books#home", as: :books_page
+
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
@@ -9,22 +22,10 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  get "books" => "books#home", as: :books_page
-
-
-  root "pages#home"
-  get "home", to: "pages#home" # redirects /home to root path
-  resources :books
-
   #Defines user profile route
   get 'profile', to: 'users#show', as: 'profile'
 
   #Defines user read books route
   get 'my_reads', to: 'users#my_reads', as: 'my_reads'
 
-  #Devise routes for users
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
 end
